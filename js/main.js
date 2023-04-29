857b29004c1d32079328ac3512137d3b7be6b8a3
Luccas Fialho <luccas.flh@gmail.com>
2023-04-29 16:34:10 -0300

    Adicionado função de alterar a quantidade de um item

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const form = document.getElementById('novoItem')
 const lista = document.getElementById('lista')
-const itens = JSON.parse(localStorage.getItem('itens'))|| []
+const itens = JSON.parse(localStorage.getItem('itens')) || []
 
 itens.forEach(elemento => {
     criaElemento(elemento)
@@ -12,16 +12,31 @@ form.addEventListener('submit', evento => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    nome.value = nome.value[0].toUpperCase() + nome.value.toLowerCase().substring(1)
+
+
+    const existe = itens.find(item => item.nome === nome.value)
+
+    console.log(existe);
+
     const itemAtual = {
         'nome': nome.value,
         'quantidade': quantidade.value
     }
 
-    itens.push(itemAtual)
+    if (existe) {
+        itemAtual.id = existe.id
+        itens[existe.id] = itemAtual
+        atualizaElemento(itemAtual)
+    } else {
+        itemAtual.id = itens.length
+        criaElemento(itemAtual)
+        itens.push(itemAtual)
+    }
 
     localStorage.setItem('itens', JSON.stringify(itens))
-    
-    criaElemento(itemAtual)
+
+
     nome.value = ''
     quantidade.value = ''
 })
@@ -29,12 +44,17 @@ form.addEventListener('submit', evento => {
 function criaElemento(item) {
     const novoItem = document.createElement('li')
     novoItem.classList.add('item')
-    
+
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
 
     novoItem.appendChild(numeroItem)
     novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
+}
+
+function atualizaElemento(item) {
+    document.querySelector(`[data-id="${item.id}"]`).innerHTML = item.quantidade;
 }
